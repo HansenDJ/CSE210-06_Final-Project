51db90216c483e403c73bb9b7745c54f5cccf8b9
HansenDJ <hansendj38@gmail.com>
2022-03-14 12:47:36 -0600

    Initialized console and raylib. Added service classes

---
+++ Game/Director.cs
@@ -0,0 +1,15 @@
+using System;
+using Raylib_cs;
+
+namespace generalNamespace;
+
+public class Director
+{
+    public void startGame()
+    {
+        while (!Raylib.WindowShouldClose())
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using generalNamespace;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Director director = new Director();
+        director.startGame();
+    }
+}
