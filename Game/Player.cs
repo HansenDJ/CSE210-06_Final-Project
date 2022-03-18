c2b39c585de20cb3ea3de6adb601cef0016036bc
HansenDJ <hansendj38@gmail.com>
2022-03-17 22:33:27 -0600

    Removed

--- Game/Player.cs
+++ Game/Player.cs
@@ -1,14 +1,8 @@
 using System;
-using CharacterNameSpace;
+
+namespace generalNamespace;
 
 public class Player : Character
 {
-    public Player(string path, int width, int height,
-        float x = 0, float y = 0,
-        float vx = 0, float vy = 0,
-        float rotation = 0, float rotationVel = 0) : 
-        base(path, width, height, x, y, vx, vy, rotation, rotationVel)
-    {
-
-    }
+    
 }
