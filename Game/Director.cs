7279260ee83d360bbc4234f1ca211685cc2db27b
dosmaster4life <jefferysuperman@gmail.com>
2022-03-17 19:41:00 -0600

    Added Timer

--- Game/Director.cs
+++ Game/Director.cs
@@ -1,18 +1,19 @@
 using System;
 using Raylib_cs;
-
+using System.Timers;
 namespace generalNamespace;
 
 public class Director
 {
-    private int fps;
+    private static System.Timers.Timer timer;
+    static bool action = false;
     public void StartGame()
     {
         VideoService vd= new();
         Player player = new();
 
         Raylib.InitWindow(VideoService.scrnWidth, VideoService.scrnHeight, "FLUX");
-        SetFPS();
+      
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
@@ -22,8 +23,15 @@ public class Director
             Raylib.EndDrawing();
         }
     }
-    public void SetFPS(int fps = 60) {
-        this.fps = fps;
-        Raylib.SetTargetFPS(fps);
+    static void SetTimer() {
+        timer = new System.Timers.Timer(20);
+
+        timer.Elapsed += OnTimedEvent;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+    }
+
+    static void OnTimedEvent(Object source, ElapsedEventArgs e) {
+        action = true;
     }
 }
