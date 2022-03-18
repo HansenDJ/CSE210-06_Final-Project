7324a57ef7a08c50de3c671e423e974f112196ca
dosmaster4life <jefferysuperman@gmail.com>
2022-03-17 22:39:54 -0600

    a

--- Game/Characters/Character.cs
+++ Game/Characters/Character.cs
@@ -4,5 +4,15 @@ public class Character
     public int x = 30;
     public int y = 30;
     public int radius = 30;
+
+    public void setX(int X)
+    {
+        x = X;
+    }
+
+    public void setY(int Y)
+    {
+        y = Y;
+    }
     
 }
