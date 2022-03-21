7728aecab2d261cd607889743dd23aa1d4874993
HansenDJ <hansendj38@gmail.com>
2022-03-21 11:59:30 -0600

    Added GetPlayerMovementSpeed method

--- Game/Player.cs
+++ Game/Player.cs
@@ -4,5 +4,11 @@ namespace generalNamespace;
 
 public class Player : Character
 {
-    
+    // Player constants
+    public static int playerMovementSpeed = 5;
+
+    public int GetPlayerMovementSpeed()
+    {
+        return playerMovementSpeed;
+    }
 }
