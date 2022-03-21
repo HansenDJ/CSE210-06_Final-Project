7dd89e049ab6016b2cd12a38b74d6c552954266d
Krisst <krisstopher03@gmail.com>
2022-03-21 12:10:51 -0600

    Earth Ships

--- Game/Services/ImageService.cs
+++ Game/Services/ImageService.cs
@@ -6,12 +6,39 @@ namespace generalNamespace;
 
 public class ImageService
 {
-    
+
     // Create methods to define in the RaylibImageService class to load the image files into textures.
-    
-    public static Image SetShipStartImage() {
+
+    public static Image SetShipStartImage()
+    {
         // Load ship image
         return LoadImage("Game/Images/ship/startship.png");
     }
-    
+
+    public static Image SetEarthOneStartImage()
+    {
+        // Load ship image for Earth LV 1
+        return LoadImage("Game/Images/ship/earthlv1.png");
+    }
+
+    public static Image SetEarthTwoStartImage()
+    {
+        // Load ship image for Earth LV 2
+        return LoadImage("Game/Images/ship/earthlv2.png");
+    }
+
+    public static Image SetEarthThreeStartImage()
+    {
+        // Load ship image for Earth LV 3
+        return LoadImage("Game/Images/ship/earthlv3.png");
+    }
+
+        public static Image SetEarthBossStartImage()
+    {
+        // Load ship image for Earth Boss
+        return LoadImage("Game/Images/ship/earthboss.png");
+    }
+//Fire
+
+
 }
