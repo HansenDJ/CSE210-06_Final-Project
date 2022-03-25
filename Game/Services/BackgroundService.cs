60cd22794a93777ae005106989fb5bdfe09a92ee
dosmaster4life <jefferysuperman@gmail.com>
2022-03-24 21:42:23 -0600

    bug fix

---
+++ Game/Services/BackgroundService.cs
@@ -0,0 +1,34 @@
+using Raylib_cs;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+namespace generalNamespace;
+
+public class BackgroundService
+{
+    private Image bg1Image;
+    private Image bg2Image;
+    public Texture2D bg1Texture;
+    public Texture2D bg2Texture;
+    public int x1 = 0;
+    public int x2 = 0;
+    public int y1 = 0;
+    public int y2 = 0;
+
+    public void Setbq1()
+    {
+        
+    }
+
+    public void Setbg2()
+    {
+        
+    }
+    public void SetTexture(Image imagebg,Image imagebg2)
+    {
+        bg1Image = imagebg;
+        bg2Image = imagebg2;
+        bg1Texture = LoadTextureFromImage(bg1Image);
+        bg2Texture = LoadTextureFromImage(bg2Image);
+    }
+
+}
--- Game/Services/VideoService.cs
+++ Game/Services/VideoService.cs
@@ -18,24 +18,25 @@ public class VideoService
     // public static int scrnWidth = Convert.ToInt32(FullPrimaryScreenWidth);
     // public static int scrnHeight = Convert.ToInt32( FullPrimaryScreenHeight);
 
-    public static void Draw(List<Enemy> objectsToDraw, List<Weapon> weaponsToDraw, Player player)
+    public static void Draw(List<Enemy> objectsToDraw, List<Weapon> weaponsToDraw,Player player,BackgroundService bg)
     {
-        DrawBackdrop();
+        DrawBackdrop(bg);
         DrawEntities(objectsToDraw,weaponsToDraw,player);
         DrawShip();
     }
 
-    private static void DrawBackdrop()
+    private static void DrawBackdrop(BackgroundService bg)
     {
         Raylib.ClearBackground(Raylib_cs.Color.BLACK);
+        DrawTexture(bg.bg1Texture,0,0,WHITE);
     }
 
-    /* QUESTION: Should end condition for for loop be -1 or not?
+    /* QUESTION: Should end be -1 or not?
     -------------------------------------
     -----------------------------------*/
-    private static void DrawEntities(List<Enemy> objectsToDraw, List<Weapon> weaponsToDraw, Player player)
+    private static void DrawEntities(List<Enemy> objectsToDraw,List<Weapon> weaponsToDraw,Player player)
     {
-        Console.WriteLine("Object Count: " + objectsToDraw.Count().ToString() + "Weapon Count" + weaponsToDraw.Count().ToString());
+        Console.WriteLine( "Object Count: " + objectsToDraw.Count().ToString() + "Weapon Count" + weaponsToDraw.Count().ToString());
         for (int i = 0; i < objectsToDraw.Count - 1; i++)
         {
             DrawEntity(objectsToDraw[i]);
@@ -45,8 +46,8 @@ public class VideoService
         {
             if (!weaponsToDraw[i].location)
             {
-                weaponsToDraw[i].SetX(player.x + player.GetColliderBoxWidth());
-                weaponsToDraw[i].SetY(player.y + player.GetColliderBoxHeight() / 2);
+                weaponsToDraw[i].SetX(player.x);
+                weaponsToDraw[i].SetY(player.y);
                 weaponsToDraw[i].location = true;
                 {
                     
@@ -86,4 +87,6 @@ public class VideoService
     {
         DrawRectangleLines(character.x + character.GetOffsetColliderWidth() / 2, character.y + character.GetOffsetColliderHeight() / 2, character.GetColliderBoxWidth(), character.GetColliderBoxHeight(), GREEN);
     }
+
+    
 }
