ca0c6468c7961ebb004f73c3ab4bcd1b0e33205f
Krisst <krisstopher03@gmail.com>
2022-03-30 22:16:09 -0600

    Word crawl grammer

--- loadScreenVisual.cs
+++ loadScreenVisual.cs
@@ -9,12 +9,12 @@ public static class loadScreenVisual
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.BLACK);
-        Raylib.DrawText("In the year 2055 a great war occured in the Galactic Empire\n" +
-                        "You were gambling in a casino and your opponent has a Royal Flush." +
-                        "\n In order to maintain freedom you must destroy enemies and collect" +
-                        "\nenough coins. If you fail to obtain the required amount of coins " +
-                        "\nyou will be sold as a slave to work in the galactic mines." +
-                        "\npush the Spacebar to begin your journey.",250,250,25,Color.WHITE);
+        Raylib.DrawText("In the year 2355 a great war occured in the Galactic Empire\n" +
+                        "You were gambling in a casino and your opponent had a Royal Flush." +
+                        "\n In order to pay off your debt and maintain freedom you must "+
+                        "\ndestroy enemies and collect enough coins. If you fail to obtain the " +
+                        "\nrequired amount of coins you will be sold as a slave to work in the galactic mines." +
+                        "\nPush the Spacebar to begin your journey.",250,250,25,Color.WHITE);
         Raylib.EndDrawing();
     }
 }
