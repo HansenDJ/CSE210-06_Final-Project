9b294b26397905f4b307f6c176fc878566398959
Roran47 <ekcook@live.com>
2022-04-03 15:49:09 -0700

    done with load screans

--- Instructions.cs
+++ Instructions.cs
@@ -11,14 +11,25 @@ public static class instructions
     {
         Raylib.BeginDrawing();
         Raylib.DrawTexture(ImageService.spacebgTexture,0,0,Color.WHITE);
-        Raylib.DrawText("Instructions", 460, 50, 75, Color.WHITE);
-        Raylib.DrawText("Fire Weapons     =   [Spacebar] or [Enter] or [Mouse 1]\n" +
-                        "Move Up            =   [W] or [^]\n" +
-                        "Move Backward   =   [A] or [<]\n" +
-                        "Move Forward    =   [D] or [>]\n" +
-                        "Move Down         =   [S] or [v]\n" +
-                        "Gamble              =   [shift]\n" +
-                        "Repair Ship        =   [Tab] or [Mouse 2]", 440, 240, 25, Color.WHITE);
+        Raylib.DrawText("Instructions", 470, 50, 75, Color.WHITE);
+        Raylib.DrawText("Move Up\n" +
+                        "Move Backward\n" +
+                        "Move Forward\n" +
+                        "Move Down\n" +
+                        "Fire Weapons\n" +
+                        "Repair Ship (-100 Coins)\n" +
+                        "Gamble (-50 Coins)" , 340, 240, 25, Color.WHITE);
+        Raylib.DrawText("=   [W] or [^]\n" +
+                        "=   [A] or [<]\n" +
+                        "=   [D] or [>]\n" +
+                        "=   [S] or [v]\n" +
+                        "=   [Mouse Left] or [Spacebar]  or [Enter]\n" +
+                        "=   [Mouse Right] or [Tab] or [H] \n" +
+                        "=   [Shift]\n" , 680, 240, 25, Color.WHITE);
+
+
+
+
 
         Raylib.DrawText("Push the [R] key to begin your journey.", 455, 650, 25, Color.WHITE);
         Raylib.EndDrawing();
